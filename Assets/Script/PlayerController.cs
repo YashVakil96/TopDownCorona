bbf08be3969f7b9a90590f69096f5ae61b06abab
joker_yash96 <yashvakil96@gmail.com>
2020-04-04 17:33:28 +0530

    Updated Unity Version

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+ #region Variables
+public float speed;
+
+private Rigidbody2D rb;
+ #endregion
+
+ #region System Methods
+
+/// <summary>
+/// Start is called on the frame when a script is enabled just before
+/// any of the Update methods is called the first time.
+/// </summary>
+void Start()
+{
+    rb=GetComponent<Rigidbody2D>();
+}//start
+
+ private void Update() {
+     if(Input.GetKey(KeyCode.UpArrow))
+     {
+        //  transform.position = transform.up*speed*Time.deltaTime;
+     }
+ }//update
+ #endregion
+
+ #region User Define Methods
+ #endregion
+
+}//class
